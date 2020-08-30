6a273e628c8f5e8850c6da5ae26e77a1ef46275c
Ayush Raj <raj.ayush012@gmail.com>
2020-08-31 04:14:15 +0530

    Added navigation functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>Hats</div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MenuItem} from '../menu-item/menu-item.component';
+import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss'
 
 export class Directory extends React.Component {
@@ -48,8 +48,8 @@ export class Directory extends React.Component {
     render(){
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} size={size} imageUrl={imageUrl}/>
+                {this.state.sections.map(({id, ...sectionProps}) => (
+                    <MenuItem key={id} {...sectionProps}/>
                     ))}            
             </div>
         )

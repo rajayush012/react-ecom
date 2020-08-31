2a295e7d085a02fc831015e22725f5db4287e7ef
Ayush Raj <raj.ayush012@gmail.com>
2020-09-01 00:03:36 +0530

    Shop page added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
-import {Homepage} from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>Hats</div>
-)
+import {Homepage} from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
       
     </div>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data'
+import PreviewCollectoin from '../../components/preview-collection/preview-collection.component';
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections : SHOP_DATA
+        }
+    }
+
+    render(){
+        const {collections} = this.state;
+
+        return(
+            <div className='shop-page'>
+                <h1>Shop page</h1>
+                {collections.map(({id, ...otherColelctionProps}) => (<PreviewCollectoin key={id} {...otherColelctionProps}/>))}
+            </div>
+            
+        )
+    }
+}
+
+export default ShopPage;
